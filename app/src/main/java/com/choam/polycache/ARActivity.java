f5f95de4892fa6a67e5029b705fc1fe497811731
Emilio <editrocchio@gmail.com>
2018-12-17 18:12:14 -0800

    finished basic ARCore integration

--- app/src/main/java/com/choam/polycache/ARActivity.java
+++ app/src/main/java/com/choam/polycache/ARActivity.java
@@ -1,10 +1,26 @@
 package com.choam.polycache;
 
+import android.net.Uri;
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
 
+import com.google.ar.core.Anchor;
+import com.google.ar.core.Frame;
+import com.google.ar.core.HitResult;
+import com.google.ar.core.Plane;
+import com.google.ar.core.Point;
+import com.google.ar.core.Trackable;
+import com.google.ar.core.TrackingState;
+import com.google.ar.sceneform.AnchorNode;
+import com.google.ar.sceneform.rendering.ModelRenderable;
 import com.google.ar.sceneform.ux.ArFragment;
+import com.google.ar.sceneform.ux.TransformableNode;
+
+import java.util.List;
 
 
 public class ARActivity extends AppCompatActivity {
@@ -14,6 +30,7 @@ public class ARActivity extends AppCompatActivity {
     private boolean isTracking = false;
     private boolean isHitting = false;
 
+    private FloatingActionButton floatingActionButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,14 +40,143 @@ public class ARActivity extends AppCompatActivity {
         arFragment = (ArFragment)
                 getSupportFragmentManager().findFragmentById(R.id.sceneform_fragment);
 
+        floatingActionButton = findViewById(R.id.floatingActionButton);
+
         // Adds a listener to the ARSceneView
         // Called before processing each frame
         arFragment.getArSceneView().getScene().addOnUpdateListener(frameTime -> {
             arFragment.onUpdate(frameTime);
             onUpdate();
         });
+
+        // Set the onclick lister for fab
+        floatingActionButton.setOnClickListener(v -> {
+            addObject(Uri.parse("model.sfb"));
+        });
+    }
+
+    //Show/hide fab
+    private void showFab(boolean enabled) {
+        if (enabled) {
+            floatingActionButton.setEnabled(true);
+            floatingActionButton.setVisibility(View.VISIBLE);
+        } else {
+            floatingActionButton.setEnabled(false);
+            floatingActionButton.setVisibility(View.GONE);
+        }
+    }
+
+    // Updates the tracking state
+    private void onUpdate() {
+        updateTracking();
+        // Check if the devices gaze is hitting a plane detected by ARCore
+        if (isTracking) {
+            if(updateHitTest()) {
+                showFab(isHitting);
+            }
+        }
+    }
+
+    // Performs frame.HitTest and returns if a hit is detected
+    private boolean updateHitTest() {
+        Frame frame = arFragment.getArSceneView().getArFrame();
+        android.graphics.Point point = getScreenCenter();
+        List<HitResult> hits;
+        boolean wasHitting = isHitting;
+        isHitting = false;
+
+        if(frame != null) {
+            hits = frame.hitTest(point.x, point.y);
+            for (HitResult hit : hits) {
+                Trackable trackable = hit.getTrackable();
+                if (trackable instanceof Plane &&
+                        ((Plane) trackable).isPoseInPolygon(hit.getHitPose())) {
+                    isHitting = true;
+                    break;
+                }
+            }
         }
 
+        return wasHitting != isHitting;
+    }
+
+    // Makes use of ARCore's camera state and returns true if the tracking state has changed
+    private boolean updateTracking() {
+        Frame frame = arFragment.getArSceneView().getArFrame();
+        boolean wasTracking = isTracking;
+        isTracking = isTracking = frame != null &&
+                frame.getCamera().getTrackingState() == TrackingState.TRACKING;
+
+        return isTracking != wasTracking;
+    }
+
+    //Returns the center of the screen
+    private android.graphics.Point getScreenCenter() {
+        View view = findViewById(android.R.id.content);
+        return new android.graphics.Point(view.getWidth() / 2, view.getHeight() / 2);
+    }
+
+    /**
+     * takes in our 3D model and performs a hit test to determine where to place it
+     * @param model Uri of of 3d sfb file.
+     */
+    private void addObject(Uri model) {
+        Frame frame = arFragment.getArSceneView().getArFrame();
+        android.graphics.Point point = getScreenCenter();
+
+        if (frame != null) {
+            List<HitResult> hits = frame.hitTest(point.x, point.y);
+            for (HitResult hit : hits) {
+                Trackable trackable = hit.getTrackable();
+                if (trackable instanceof Plane &&
+                        ((Plane) trackable).isPoseInPolygon(hit.getHitPose())) {
+                    placeObject(arFragment, hit.createAnchor(), model);
+                    break;
+                }
+            }
+        }
+    }
+
+    /**
+     * @param fragment our fragment
+     * @param anchor ARCore anchor from the hit test
+     * @param model our 3D model of choice
+     *
+     * Uses the ARCore anchor from the hitTest result and builds the Sceneform nodes.
+     * It starts the asynchronous loading of the 3D model using the ModelRenderable builder.
+     */
+    private void placeObject(ArFragment fragment, Anchor anchor, Uri model) {
+        ModelRenderable.builder()
+                .setSource(fragment.getContext(), model)
+                .build()
+                .thenAccept(renderable -> addNodeToScene(fragment, anchor, renderable))
+                .exceptionally((throwable -> {
+                    AlertDialog.Builder builder = new AlertDialog.Builder(this);
+                    builder.setMessage(throwable.getMessage())
+                            .setTitle("Codelab error!");
+                    AlertDialog dialog = builder.create();
+                    dialog.show();
+                    return null;
+                }));
+    }
+
+    /**
+     * @param fragment our fragment
+     * @param anchor ARCore anchor
+     * @param renderable our model created as a Sceneform Renderable
+     *
+     * This method builds two nodes and attaches them to our scene
+     * The Anchor nodes is positioned based on the pose of an ARCore Anchor. They stay positioned in the sample place relative to the real world.
+     * The Transformable node is our Model
+     * Once the nodes are connected we select the TransformableNode so it is available for interactions
+     */
+    private void addNodeToScene(ArFragment fragment, Anchor anchor, ModelRenderable renderable) {
+        TransformableNode node = new TransformableNode(fragment.getTransformationSystem());
+        AnchorNode anchorNode = new AnchorNode(anchor);
+        node.setRenderable(renderable);
+        node.setParent(anchorNode);
+        fragment.getArSceneView().getScene().addChild(anchorNode);
+        node.select();
     }
 
 }
